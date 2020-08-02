be479ba9c43afbc537003bbdd58f2e31cc7f7474
nOrestis <orestesnalbadis@gmail.com>
2020-08-02 12:54:54 +0200

    black is back [formater]

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,4 +1,3 @@
-
 import os
 from pytube import Playlist, YouTube
 import re
@@ -10,22 +9,25 @@ import random
 def file_path():
     home = os.path.expanduser("~")
     # Create random names for each download folder to avoid existing directories creation
-    new_folder = "Downloads/youtube_videos_%s" % str(random.randint(0,1000))   
-    download_folder = os.path.join(home,new_folder)
+    new_folder = "Downloads/youtube_videos_%s" % str(random.randint(0, 1000))
+    download_folder = os.path.join(home, new_folder)
     access_rights = 0o755
     try:
-        os.mkdir(download_folder,access_rights)
+        os.mkdir(download_folder, access_rights)
     except OSError:
-        print ("Creation of the directory %s failed" % download_folder)
+        print("Creation of the directory %s failed" % download_folder)
     else:
-        print ("Successfully created the directory %s" % download_folder)
+        print("Successfully created the directory %s" % download_folder)
     return download_folder
 
-def download_video(url=None,playlist_url=None):
+
+def download_video(url=None, playlist_url=None):
     path = file_path()
     if url:
-        yt_video= YouTube(url)#
-        yt_video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download(output_path=path)
+        yt_video = YouTube(url)  #
+        yt_video.streams.filter(progressive=True, file_extension="mp4").order_by(
+            "resolution"
+        )[-1].download(output_path=path)
         print("Video downloaded succesfully")
     elif playlist_url:
         playlist = Playlist(playlist_url)
@@ -33,10 +35,12 @@ def download_video(url=None,playlist_url=None):
 
         amount_of_videos = len(playlist.video_urls)
         print("Number of videos in playlist: %s" % amount_of_videos)
-        for video,number in zip(playlist,range(0,amount_of_videos)):
+        for video, number in zip(playlist, range(0, amount_of_videos)):
             yt = YouTube(video)
-            numbered_title = str(number+1) + ") " + yt.title
-            yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download(filename=numbered_title,output_path=path)
+            numbered_title = str(number + 1) + ") " + yt.title
+            yt.streams.filter(progressive=True, file_extension="mp4").order_by(
+                "resolution"
+            )[-1].download(filename=numbered_title, output_path=path)
             print("Video #{} downloaded".format(number))
 
 
@@ -47,5 +51,3 @@ if str(choice) == "v":
 elif str(choice) == "p":
     playlist_url = input("Enter the link of the playlist: ")
     download_video(playlist_url=playlist_url)
-
-
